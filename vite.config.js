6cc016a4c4345613c6eff3f6ef3d03072a19157f
shakin-shahria <shakinshaharia@gmail.com>
2025-02-02 17:26:12 +0600

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
-  },
+  base: "/DevPortfolio/", // ✅ Make sure this matches your GitHub repo name
 });
